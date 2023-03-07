b2e0fcfc02c5c82d4341ac56601fe68a766b1444
Thomas Michelot <thom.michelot@free.fr>
2023-03-07 20:29:04 +0100

    feat: base code
    Signed-off-by: Thomas Michelot <thom.michelot@free.fr>

---
+++ app.py
@@ -0,0 +1,50 @@
+"""
+This module will serve as automatic notification to watch latest version of hamachi for linux.
+"""
+
+import time
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+options = webdriver.ChromeOptions()
+driver = webdriver.Remote(
+    command_executor="http://localhost:4444/wd/hub", options=options
+)
+
+URL = "https://vpn.net/linux"
+
+# Navigate to URL
+driver.get(URL)
+time.sleep(1)
+
+# XPath for 64bit version
+XPATH = '//*[@id="lb_hamachiLinuxMore"]/div[2]/a[3]'
+
+current_version = driver.find_element(By.XPATH, XPATH).text
+
+# Close the browser
+driver.close()
+driver.quit()
+
+# Trasform text to get desired format
+current_version = current_version.replace("logmein-hamachi-", "")
+current_version = current_version.replace("-x64.tgz", "")
+current_version = current_version.strip()
+
+# Get last checked version (should only contain one line)
+with open("last.txt", "r", encoding="utf-8") as f:
+    last_version = f.readlines()[0].strip()
+
+print(f"Last    version: {last_version}")
+print(f"Current Version: {current_version}")
+
+if last_version == current_version:
+    print("No new version.")
+else:
+    print("There is a new version.")
+    print(f"Last version: {last_version}")
+    print(f"New  version: {current_version}")
+    print("Saving current version as last version.")
+    with open("last.txt", "w", encoding="utf-8") as f:
+        f.write(f"{current_version}")
